466dcb73f3333cea4ca18061bf6fac1817fb3014
patricksachaj <patricksachaj2019@u.northwestern.edu>
2019-05-17 17:03:19 -0500

    First round

---
+++ src/lib.rs
@@ -0,0 +1,94 @@
+
+const ticwins: [[usize; 3]; 8] = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9]];
+
+
+pub fn lets_play(game: usize, diff: usize)
+{
+    match game
+        {
+            1 => starttic(),
+            2 => println!("connect4"),
+            3 => println!("checkers"),
+            _ => println!("error"),
+        }
+}
+
+struct ticgame<'a>
+{
+    board: Vec<&'a str>,
+}
+
+impl<'a> ticgame<'a>
+{
+    fn new() -> Self
+    {
+        ticgame{board: vec![""; 9]}
+    }
+
+    fn printboard(& mut self)
+    {
+        println!("  1  2  3");
+        println!("A {}  {}  {}", self.board[0], self.board[1], self.board[2]);
+        println!("B {}  {}  {}", self.board[3], self.board[4], self.board[5]);
+        println!("C {}  {}  {}", self.board[6], self.board[7], self.board[8]);
+    }
+
+    fn validmove(& mut self, row: &'a str, col: usize) -> bool
+    {
+        if row == "A" || row == "B" || row == "C"
+        {
+            if col == 1 || col == 2 || col == 3
+            {
+                let int = match row
+                    {
+                        "A" => 0,
+                        "B" => 3,
+                        "C" => 6,
+                        _ => 100,
+                    };
+
+                if self.board[int + col] == ""
+                {
+                    true;
+                }
+            }
+        }
+
+        false
+    }
+
+    fn check_win(& mut self) -> bool
+    {
+        let mut X = Vec::new();
+        let mut O = Vec::new();
+
+        for (index, symbol) in self.board.iter().enumerate() //starts at 1
+        {
+            if symbol == &"X"
+            {
+                X.push(index);
+            }
+
+            if symbol == &"O"
+            {
+                O.push(index);
+            }
+        }
+
+        for vecs in ticwins
+            {
+                if
+            }
+
+        false
+    }
+}
+
+fn starttic()
+{
+    let mut new_game = ticgame::new();
+    new_game.printboard();
+    println!("Where do you want to put your X? (row then column)")
+
+}
+
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use std::io::{prelude::*, self};
+use std::io::{stdin,stdout,Write};
+use std::process;
+use finalproject::lets_play;
+
+
+fn main() -> std::io::Result<()> {
+    println!("Welcome to Gamer's Paradise! What would you like to play: Tic-Tac-Toe, Connect4, or Checkers?");
+    let mut game= String::new();
+    let _ = stdout().flush();
+    stdin().read_line(&mut game).expect("Did not enter a correct string");
+    let game = game.to_lowercase();
+    let mut game_num = 0;
+    if game.contains("tic")
+    {
+        game_num = 1;
+    }
+    else if game.contains("connect4")
+    {
+        game_num = 2;
+    }
+    else if game.contains("checkers")
+    {
+        game_num = 3;
+    }
+    else {
+        eprintln!("Sorry, we do not offer that game! BYE!");
+        process::exit(1);
+    }
+    println!("What difficulty would you like to play? Easy, medium, or hard");
+    let mut difficulty= String::new();
+    let _ = stdout().flush();
+    stdin().read_line(&mut difficulty).expect("Did not enter a correct string");
+    let difficulty = difficulty.to_lowercase();
+    let mut difficulty_num = 0;
+    if difficulty.contains("easy")
+    {
+        difficulty_num = 1;
+    }
+    else if difficulty.contains("medium")
+    {
+        difficulty_num = 2;
+    }
+    else if difficulty.contains("hard")
+    {
+        difficulty_num = 3;
+    }
+    else {
+        eprintln!("Sorry, we do not offer that difficulty! BYE!");
+        process::exit(1);
+    }
+
+    lets_play(game_num, difficulty_num);
+
+    Ok(())
+}
