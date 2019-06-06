307fe029fcc220b265343bcaa65c592cbd9d2b75
rachelseng <rachelseng@yahoo.com.sg>
2019-06-06 14:54:40 -0500

    doc comment examples for lib

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod minimax;
 
 
 #[allow(unused)]
-/// Game tree of possible moves with values of heuristic evaluations.
+/// A trait for the ability to make a game tree of possible moves with values of heuristic evaluations.
 ///
 /// A heuristic is an approximation that is typically fast and used to aid in optimization problems.
 /// In this context, heuristics are used to “rate” board positions based on local information.
@@ -15,6 +15,38 @@ pub mod minimax;
 /// the heuristic function is generally referred to as the evaluation function, or the static
 /// evaluator. The static evaluation takes in a board position, and gives it a score.
 /// The higher the score, the better it is for you, the lower, the better for the opponent.
+
+/// # Derivable
+/// This trait can be used with #[derive] if all fields are HeuristicGameTree.
+/// ```
+/// // `derive` implements HeuristicGameTree for Games.
+/// #[derive(HeuristicGameTree)]
+/// enum Games {
+///    mancala,
+///    chess,
+///    tictactoe,
+///    checkers
+/// }
+/// ```
+///
+/// # How can I implement HeuristicGameTree?
+/// HeuristicGameTree requires the possible_moves, heuristic, execute_move methods to be implemented.
+/// An example implementation for a connect4 game is:
+/// ```
+/// use crate::heuristic_game_tree::HeuristicGameTree;
+/// impl<'a> HeuristicGameTree for Connect4Game {
+///     type Move = usize;
+///
+///     /// Returns a box that contains a pointer to an iterator of all the possible moves
+///     fn possible_moves(&self) -> Box<Iterator<Item = Self::Move> + '_> {...}
+///
+///     /// Returns an isize that represents the heuristic value (i.e. how good/bad the move is)
+///     fn heuristic(&self) -> isize {...}
+///
+///     /// Returns a boolean affirming/ denying whether the move has been made
+///     fn execute_move(&mut self, next_move: &Self::Move, is_opponent: bool) -> bool{...}
+/// }
+/// ```
 pub trait HeuristicGameTree: Clone {
     type Move: Clone + Sized;
     //type Heuristic: PartialOrd;
